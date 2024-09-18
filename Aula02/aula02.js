5f6e8d7156d94a021383c8d0ed99fa5ca8f18bc7
Kacaii <kacaii.privado@gmail.com>
2024-09-18 14:05:35 -0300

    updated aula02.js

--- Aula02/aula02.js
+++ Aula02/aula02.js
@@ -1,9 +1,8 @@
 var prompt = require("prompt-sync")();
-
 let listaItensDisponiveis = new Set(["garrafa", "coxinha", "tapioca"]);
-console.table(listaItensDisponiveis, [1]);
 
-let itemEscolhido = prompt("O que você gostaria de comprar? ");
+console.table(listaItensDisponiveis);
+let itemEscolhido = prompt("O que você gostaria de comprar? "); // Recebe input do usuário
 
 let verificaCompra = () => {
   if (itemEscolhido == null) return; // Adicionando guard clauses
@@ -12,8 +11,6 @@ let verificaCompra = () => {
 
 function comprarItem() {
   if (verificaCompra()) {
-    listaItensDisponiveis.delete(itemEscolhido);
-
     console.log(`${itemEscolhido} foi adicionado ao seu inventário!`);
     return;
   }
