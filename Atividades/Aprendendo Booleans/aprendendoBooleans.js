89226c05e9c3553953d3ed37d21d63e99b7b8fa0
KacaiiSENAC <kacaii.privado@gmail.com>
2024-10-02 09:59:23 -0300

    exercicio finalizado

--- Atividades/Aprendendo Booleans/aprendendoBooleans.js
+++ Atividades/Aprendendo Booleans/aprendendoBooleans.js
@@ -1,4 +1,16 @@
-const porta = true;
-const verificaPorta = () => (porta ? "aberta! " : "fechada! ");
+const portaAberta = true;
+const verificaPorta = () => (portaAberta ? "aberta! " : "fechada! ");
 
 console.log(`A porta esta ${verificaPorta()}`);
+
+const podeEntrar = prompt("Eu posso entrar?");
+
+function verificaEntrada() {
+  return podeEntrar === "pode" && portaAberta;
+}
+
+function tentaEntrar() {
+  return verificaEntrada() ? "Entrei! 󰩈" : "Nao entrei ";
+}
+
+console.log(tentaEntrar());
