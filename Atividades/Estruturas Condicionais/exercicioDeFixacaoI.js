bbcfa0bc0ac0c7463c2ff7e6cee8154520d038bb
Kacaii <kacaii.privado@gmail.com>
2024-10-06 17:37:00 -0300

    atividade 1 finalizada

---
+++ Atividades/Estruturas Condicionais/exercicioDeFixacaoI.js
@@ -0,0 +1,50 @@
+const paciente = {
+  nome: null,
+  idade: null,
+  apenasAcompanhado: false,
+  possuiAcompanhante: false,
+  servicoDesejado: null,
+};
+
+console.clear();
+
+console.log("Bem vindo(a) ao Sistema Virtual de Saúde! \n");
+paciente.nome = prompt("Qual o nome do beneficiário? \n \n>");
+
+if (!paciente.nome) throw new Error("O nome não pode ficar vazio!");
+
+console.clear();
+
+paciente.idade = prompt(
+  `Prazer em te conhecer, ${paciente.nome}! Qual seria a sua idade? \n \n>`,
+);
+
+if (!paciente.idade || isNaN(paciente.idade))
+  throw new Error("Idade inválida! Vamos tentar de novo?");
+
+console.clear();
+
+if (paciente.idade < 18) {
+  paciente.apenasAcompanhado = true;
+  paciente.possuiAcompanhante = confirm(
+    `${paciente.nome}, vi que você tem apenas ${paciente.idade} anos, você possui algum(a) acompanhante para ir com você? \n \n`,
+  );
+}
+
+if (paciente.apenasAcompanhado && !paciente.possuiAcompanhante)
+  throw new Error("Infelizmente você precisa de um acompanhante.");
+
+console.clear();
+
+paciente.servicoDesejado = prompt(
+  "Qual profissional de saúde você procura? \n \n>",
+);
+
+if (!paciente.servicoDesejado)
+  throw new Error("Este campo não pode ficar vazio!");
+
+console.clear();
+
+console.info(
+  `${paciente.servicoDesejado} encontrado(a)! Mandamos os detalhes para o seu email <3`,
+);
