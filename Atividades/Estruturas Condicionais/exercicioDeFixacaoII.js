4cc31fdcf767752d930dc3e0bde468c11272a68f
Kacaii <kacaii.privado@gmail.com>
2024-10-06 17:55:54 -0300

    atividade 2 finalizada

---
+++ Atividades/Estruturas Condicionais/exercicioDeFixacaoII.js
@@ -0,0 +1,29 @@
+console.clear();
+
+const listaDeComidas = ["Banana", "Laranja", "Beterraba"];
+
+console.table(listaDeComidas);
+const frutaDesejada = prompt("Escolha uma das opções: \n \n>");
+
+if (!frutaDesejada) throw new Error("Você esqueceu de digitar a fruta.");
+
+switch (frutaDesejada.toLowerCase()) {
+  case "banana":
+    console.clear();
+    console.info("Rica em potássio!");
+    break;
+
+  case "laranja":
+    console.clear();
+    console.info("Rica em vitamina C!");
+    break;
+
+  case "beterraba":
+    console.clear();
+    console.info("Faz bem pra circulação!");
+    break;
+
+  default:
+    console.warn("Precisa ser um item da lista!");
+    break;
+}
