7d0fa4aea9bb1966babefcf7edfcf5763d913a62
Kacaii <kacaii.privado@gmail.com>
2024-10-16 15:54:52 -0300

    Add JSDocs

--- Atividades/Estruturas Condicionais/exercicioDeFixacaoII.js
+++ Atividades/Estruturas Condicionais/exercicioDeFixacaoII.js
@@ -1,9 +1,26 @@
 console.clear();
+
+/**
+ * Array de comidas disponíveis.
+ * @type {string[]}
+ */
 const listaDeComidas = ["Banana", "Laranja", "Beterraba"]; // Apresentadas em uma tabela
 
 console.table(listaDeComidas);
+
+/**
+ * Solicita ao usuário que insira o nome da fruta desejada e guarda em uma variável
+ * @type {string | null}
+ */
 const inputFruta = prompt("\nInsira o nome da fruta escolhida: \n \n>");
 
+/**
+ * Verifica o input fornecido pelo usuário.
+ * Garante que o input não seja vazio e converte a fruta para letras minúsculas.
+ *
+ * @param {string | null} inputFruta - O nome da fruta fornecida pelo usuário.
+ * @returns {[string | null, Error | null]} Retorna a fruta em minúsculas ou um erro se o input for inválido.
+ */
 const verificaInput = (inputFruta) => {
   if (inputFruta === null || inputFruta.trim() === "") {
     return [null, new Error("Input vazio!  ")];
@@ -13,12 +30,22 @@ const verificaInput = (inputFruta) => {
 };
 
 console.clear();
+
+/**
+ * Armazena o resultado da verificação do input do usuário.
+ * @type {[string | null, Error | null]}
+ */
 const [frutaDesejada, error] = verificaInput(inputFruta);
 
 if (error) {
   console.error(error.message);
 } else {
   console.log("\n");
+
+  /**
+   * Exibe informações nutricionais dependendo da fruta escolhida.
+   * Se a fruta não estiver na lista, exibe um aviso.
+   */
   switch (frutaDesejada) {
     case "banana":
       console.clear();
