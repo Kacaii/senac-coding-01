fbcc11a442b7c0ce45f925db74fe86a5d64cab1c
KacaiiSENAC <kacaii.privado@gmail.com>
2024-10-16 09:40:41 -0300

    Small tweaks

--- Atividades/Aprendendo Booleans/aprendendoBooleans.js
+++ Atividades/Aprendendo Booleans/aprendendoBooleans.js
@@ -1,18 +1,17 @@
+// Defina o estado da porta aqui
 const portaAberta = true;
+const respostasPossiveis = new Set(["sim", "pode", "entre", "venha, chegue"]);
 const verificaPorta = () => (portaAberta ? "aberta! " : "fechada! ");
 
-const respostasPossiveis = new Set(["sim", "pode", "entre", "venha"]);
+console.clear();
+console.log(`\n A porta esta ${verificaPorta()}`);
 
-console.log(` A porta esta ${verificaPorta()}`);
-
-const pedePraEntrar = prompt("Eu posso entrar?").toLocaleLowerCase();
+const pedePraEntrar = prompt("Eu posso entrar? \n\n>").toLocaleLowerCase();
 
 function verificaEntrada() {
   return respostasPossiveis.has(pedePraEntrar) && portaAberta;
 }
 
-function tentaEntrar() {
-  return verificaEntrada() ? "Entrei! 󰩈" : "Não consegui entrar ";
-}
-
-console.log(tentaEntrar());
+verificaEntrada()
+  ? console.log("%cEntrei! 󰩈", "color:green")
+  : console.log("%cNão consegui entrar ", "color:red");
