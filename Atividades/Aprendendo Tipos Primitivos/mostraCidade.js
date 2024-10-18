0fa06a8e7c5dc18eb03671c4aab4c13c126777fd
KacaiiSENAC <kacaii.privado@gmail.com>
2024-10-18 11:03:43 -0300

    Add JSDocs

--- Atividades/Aprendendo Tipos Primitivos/mostraCidade.js
+++ Atividades/Aprendendo Tipos Primitivos/mostraCidade.js
@@ -1,5 +1,10 @@
 // Atividade 02
 
+/**
+ * Mostra o nome e a cidade que foram passados como argumentos.
+ * @param {string} - Nome da pessoa.
+ * @param {string} - Nome da cidade onde a pessoa mora.
+ */
 function mostraCidade(nome = "Pedro", cidade = "Olinda") {
   console.log(`${nome} é de ${cidade}`);
 }
