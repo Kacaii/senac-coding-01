3d7b434aa6a703f4ece483692c90f89bdbb9687f
Kacaii <kacaii.privado@gmail.com>
2024-10-19 12:14:15 -0300

    Add @example to some of the JSDocs

--- Atividades/Aprendendo Tipos Primitivos/calculaArea.js
+++ Atividades/Aprendendo Tipos Primitivos/calculaArea.js
@@ -1,13 +1,23 @@
 // Atividade 04
 
 /**
+ * Calcula a área de um triângulo utilizando os argumentos passados.
+ *
  * @param {number} base -  Base do triângulo
  * @param {number} altura -  Altura do triângulo
  * @returns {number} Retorna a área do triângulo
+ *
+ * @example Um exemplo de como esta função deve ser utilizada
+ *
+ * ```js
+ * calculaAreaDoTriangulo(9, 18) // 81.00
+ *
+ * ```
  */
 function calculaAreaDoTriangulo(base, altura) {
   return (base * altura) / 2;
 }
 
 const areaDoTriangulo = calculaAreaDoTriangulo(9, 18);
+
 console.log(`A área to triângulo é igual a ${areaDoTriangulo.toFixed(2)}`);
--- Atividades/Aprendendo Tipos Primitivos/calculaDesconto.js
+++ Atividades/Aprendendo Tipos Primitivos/calculaDesconto.js
@@ -1,14 +1,21 @@
 // Atividade 06
 
 /**
- * Calcula o valor de uma compra com desconto
- * @param {number} valorOriginal - Valor original do produto antes do desconto
+ * Calcula o valor de uma compra com desconto aplicado
+ * @param {number} valorOriginal - Valor original do produto *antes* do desconto
  * @param {number} percentualDeDesconto - Valor do desconto em %
  * @returns {number} Retorna o valor da compra com o desconto aplicado
+ *
+ * @example Um exemplo de como esta função deve ser utilizada
+ *
+ * ```js
+ * calculaValorComDesconto(100, 10) // 90
+ *
+ * ```
  */
 function calculaValorComDesconto(valorOriginal, percentualDeDesconto) {
   return valorOriginal - valorOriginal * (percentualDeDesconto / 100);
 }
 
 const resultado = calculaValorComDesconto(100, 10);
-console.log(`Seu desconto é de ${resultado}`);
+console.log(`Seu desconto é de $${resultado}`);
