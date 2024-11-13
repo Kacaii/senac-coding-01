55a56fc4724404b2d6d3c30bc9de128500821300
Kacaii <kacaii.privado@gmail.com>
2024-11-13 14:36:39 -0300

    Changed Number() to parseInt()

--- Coding1/Atividades/Aprendendo Tipos Primitivos/calculaMedia.js
+++ Coding1/Atividades/Aprendendo Tipos Primitivos/calculaMedia.js
@@ -1,19 +1,20 @@
 // Atividade 05
 
+console.clear(); // Limpando
+
 /**
- * Calcula a media das notas, independente do número de **argumentos** passados
+ * Calcula a media das notas, independente do número de **argumentos** passados.
  * @param {number[]} notas - Notas do aluno
  * @returns {number} Retorna a média das notas
  */
 function calcularMedia(...notas) {
-  const media = (
+  const media =
     notas.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0) /
-    notas.length
-  ).toFixed(2);
+    notas.length;
 
-  return Number(media);
+  return parseInt(media.toFixed(2)); // `.toFixed()` retorna uma string.
 }
 
 // Tente com mais argumentos, funciona até com mais de 3 notas
-const resultado = calcularMedia(10, 3, 7, 10);
+const resultado = calcularMedia(10, 3, 7, 2, 9);
 console.log(`Sua média é de ${resultado} `);
