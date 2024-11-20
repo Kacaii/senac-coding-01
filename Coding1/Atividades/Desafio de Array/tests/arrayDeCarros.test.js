c063c96fa2755022d5134111aba525ed724e9ffb
Kacaii <kacaii.privado@gmail.com>
2024-11-20 08:03:33 -0300

    rename folder

--- Coding1/Atividades/Desafio de Array/tests/arrayDeCarros.test.js
+++ Coding1/Atividades/Desafio de Array/tests/arrayDeCarros.test.js
@@ -6,14 +6,18 @@ console.log = () => {}; // Travando console.log
 console.table = () => {}; // Travando console.table
 console.clear = () => {}; // Travando console.clear
 
-Deno.test("Validando o método adicionarCarro", async (t) => {
+Deno.test("Validando se a lista inicia vazia", () => {
+  assertEquals(LocadoraDeCarros.listaParaInteragir, [], "Lista não vazia!");
+});
+
+Deno.test("Validando o método adicionarCarro()", async (t) => {
   await t.step("Adicionando com input vazio", () => {
     LocadoraDeCarros.listaParaInteragir = [];
     LocadoraDeCarros.adicionarCarro("");
     assertEquals(LocadoraDeCarros.listaParaInteragir, [], "Lista não vazia!");
   });
 
-  await t.step("Adicionando o carro 1", () => {
+  await t.step("Adicionando o Carro 1", () => {
     LocadoraDeCarros.listaParaInteragir = [];
     LocadoraDeCarros.adicionarCarro("Carro 1");
     assertEquals(
@@ -23,7 +27,7 @@ Deno.test("Validando o método adicionarCarro", async (t) => {
     );
   });
 
-  await t.step("Adicionando o carro 2", () => {
+  await t.step("Adicionando o Carro 2", () => {
     LocadoraDeCarros.adicionarCarro("Carro 2");
     assertEquals(
       LocadoraDeCarros.listaParaInteragir,
@@ -32,7 +36,7 @@ Deno.test("Validando o método adicionarCarro", async (t) => {
     );
   });
 
-  await t.step("Adicionando o carro 3", () => {
+  await t.step("Adicionando o Carro 3", () => {
     LocadoraDeCarros.adicionarCarro("Carro 3");
     assertEquals(
       LocadoraDeCarros.listaParaInteragir,
@@ -42,7 +46,7 @@ Deno.test("Validando o método adicionarCarro", async (t) => {
   });
 });
 
-Deno.test("Validando o método removerCarro", async (t) => {
+Deno.test("Validando o método removerCarro()", async (t) => {
   await t.step("Removendo com input vazio", () => {
     LocadoraDeCarros.listaParaInteragir = ["Carro 1", "Carro 2", "Carro 3"];
     LocadoraDeCarros.removerCarro("");
@@ -53,7 +57,7 @@ Deno.test("Validando o método removerCarro", async (t) => {
     );
   });
 
-  await t.step("Removendo o carro 1", () => {
+  await t.step("Removendo index 0", () => {
     LocadoraDeCarros.listaParaInteragir = ["Carro 1", "Carro 2", "Carro 3"];
     LocadoraDeCarros.removerCarro("0");
     assertEquals(
@@ -63,7 +67,7 @@ Deno.test("Validando o método removerCarro", async (t) => {
     );
   });
 
-  await t.step("Removendo o carro 2", () => {
+  await t.step("Removendo index 1", () => {
     LocadoraDeCarros.listaParaInteragir = ["Carro 1", "Carro 2", "Carro 3"];
     LocadoraDeCarros.removerCarro("1");
     assertEquals(
@@ -73,7 +77,7 @@ Deno.test("Validando o método removerCarro", async (t) => {
     );
   });
 
-  await t.step("Removendo o carro 3", () => {
+  await t.step("Removendo index 2", () => {
     LocadoraDeCarros.listaParaInteragir = ["Carro 1", "Carro 2", "Carro 3"];
     LocadoraDeCarros.removerCarro("2");
     assertEquals(
