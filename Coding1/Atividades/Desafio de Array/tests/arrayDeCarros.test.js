a4aea13430bf523cda12d153cc9c147b67f33f67
Kacaii <kacaii.privado@gmail.com>
2024-11-23 13:27:09 -0300

    add more tests

--- Coding1/Atividades/Desafio de Array/tests/arrayDeCarros.test.js
+++ Coding1/Atividades/Desafio de Array/tests/arrayDeCarros.test.js
@@ -17,7 +17,9 @@ Deno.test("Validando se a lista inicia vazia", () => {
 
 Deno.test("Validando o método limparLista()", async (t) => {
   await t.step("Limpando lista para interagir", () => {
+    // Limpando lista para interagir
     minhaLocadora.limparLista();
+
     assertEquals(
       minhaLocadora.listaParaInteragir.length,
       0,
@@ -26,18 +28,16 @@ Deno.test("Validando o método limparLista()", async (t) => {
   });
 });
 
-Deno.test({
-  name: "Validando o método carregarLista()",
-  permissions: {
-    read: true,
-  },
-  fn: async (t) => {
-    await t.step("Importando arquivo JSON", async () => {
-      minhaLocadora.limparLista();
+Deno.test("Validando metodo carregarLista()", async (t) => {
+  await t.step("Importando arquivo JSON válido", async () => {
+    // Limpando lista para interagir
+    minhaLocadora.limparLista();
 
-      // Importanto lista
-      await minhaLocadora.carregarLista("./api/carros_1.json");
-      assertEquals(minhaLocadora.listaParaInteragir, [
+    // Importanto lista
+    await minhaLocadora.carregarLista("./api/carros_1.json");
+    assertEquals(
+      minhaLocadora.listaParaInteragir,
+      [
         "Toyota Corolla",
         "Ford Mustang",
         "Tesla Model S",
@@ -63,43 +63,65 @@ Deno.test({
         "Bugatti Chiron",
         "McLaren 720S",
         "Toyota Land Cruiser",
-      ]);
-    });
-  },
+      ],
+      "Lista diferente do esperado!",
+    );
+  });
 });
 
 Deno.test("Validando o método adicionarCarro()", async (t) => {
   await t.step("Input vazio não adiciona nada", () => {
+    // Limpando lista para interagir
     minhaLocadora.limparLista();
+
     minhaLocadora.adicionarCarro("");
     assertEquals(minhaLocadora.listaParaInteragir, [], "Lista não vazia!");
   });
 
   await t.step("Adicionando o Carro 1", () => {
+    // Limpando lista para interagir
     minhaLocadora.limparLista();
-    minhaLocadora.adicionarCarro("Carro 1");
+
+    assertEquals(
+      minhaLocadora.adicionarCarro("Carro 1"),
+      "Carro 1",
+      "Função adicionarCarro() retornou diferente do esperado!",
+    );
+
     assertEquals(
       minhaLocadora.listaParaInteragir,
       ["Carro 1"],
-      "Carro não adicionado!",
+      "Carro 1 não adicionado!",
     );
   });
 
   await t.step("Adicionando o Carro 2", () => {
-    minhaLocadora.adicionarCarro("Carro 2");
+    // Adicionando mais um
+    assertEquals(
+      minhaLocadora.adicionarCarro("Carro 2"),
+      "Carro 2",
+      "Função adicionarCarro() retornou diferente do esperado!",
+    );
+
     assertEquals(
       minhaLocadora.listaParaInteragir,
       ["Carro 1", "Carro 2"],
-      "Carro não adicionado!",
+      "Carro 2 não adicionado!",
     );
   });
 
   await t.step("Adicionando o Carro 3", () => {
-    minhaLocadora.adicionarCarro("Carro 3");
+    // Adicionando mais um
+    assertEquals(
+      minhaLocadora.adicionarCarro("Carro 3"),
+      "Carro 3",
+      "Função adicionarCarro() retornou diferente do esperado!",
+    );
+
     assertEquals(
       minhaLocadora.listaParaInteragir,
       ["Carro 1", "Carro 2", "Carro 3"],
-      "Carro não adicionado!",
+      "Carro 3 não adicionado!",
     );
   });
 
