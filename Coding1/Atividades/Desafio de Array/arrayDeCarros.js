bad2d72266c2362afd66f34249d95c77f385b2de
Kacaii <kacaii.privado@gmail.com>
2024-11-22 23:14:41 -0300

    add spinner

--- Coding1/Atividades/Desafio de Array/arrayDeCarros.js
+++ Coding1/Atividades/Desafio de Array/arrayDeCarros.js
@@ -1,8 +1,13 @@
 import { parseArgs } from "@std/cli";
+import { Spinner } from "@std/cli/unstable-spinner";
 import { LocadoraDeCarros } from "./controllers/LocadoraDeCarros.js";
 
 const minhaLocadora = new LocadoraDeCarros(); // Instanciando nova locadora.
 const args = parseArgs(Deno.args, { alias: { help: "h", data: "d" } });
+const spinner = new Spinner({
+  message: "Carregando lista de carros...",
+  color: "yellow",
+});
 
 console.clear(); // Limpando a tela.
 
@@ -13,13 +18,19 @@ if (args.help) {
 
 if (args.data) {
   await minhaLocadora.carregarLista(args.data);
+  spinner.start(); // Apenas por questões de estética.
 } else {
   minhaLocadora.help();
   throw new Deno.errors.InvalidData("Nenhum arquivo de dados fornecido.");
 }
 
-minhaLocadora.iniciarRemocaoDeCarros(); // Iniciando
-minhaLocadora.iniciarAdicaoDeCarros();
-minhaLocadora.exibirLista();
+setTimeout(() => {
+  spinner.stop();
 
-console.log("\n%c -- == Script finalizado! == --\n", "color:yellow;");
+  minhaLocadora.iniciarRemocaoDeCarros(); // Iniciando
+  minhaLocadora.iniciarAdicaoDeCarros();
+  minhaLocadora.exibirLista();
+
+  console.log("\n%c -- == Script finalizado! == --\n", "color:yellow;");
+  // Fim!
+}, 1000);
