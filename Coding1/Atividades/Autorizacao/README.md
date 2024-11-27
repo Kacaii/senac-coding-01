6ab98c40c0df4ac51be41cc2faad4e25c5a025bb
KacaiiVM <kacaii.privado@gmail.com>
2024-11-27 09:52:08 -0300

    add types

--- Coding1/Atividades/Autorizacao/README.md
+++ Coding1/Atividades/Autorizacao/README.md
@@ -1,6 +1,6 @@
 # Atividade 27/11/2024
 
-<!-- TODO: Adicionar pontuacao e assentos -->
+<!-- TODO: Adicionar pontuacao e acentos -->
 
 > Como exercicio de casa, elabore um algoritmo que reproduza acoes do seu projeto
 > integrador com pelo menos 3 funcoes.
--- Coding1/Atividades/Autorizacao/main.js
+++ Coding1/Atividades/Autorizacao/main.js
@@ -1,6 +1,30 @@
+// TODO: Adicionar pontuacao e acentos
+
+/** @typedef {"Cliente" | "Funcionário"} Usuario */
+
 /**
- * Realiza o atendimento do usuário
- * @param {string} usr - "Cliente ou Funcionário"
- * @returns {void} Nao retorna nada.
+ * Realiza o atendimento do usuário.
+ *
+ * @param {Usuario} usr - O tipo de usuário, deve ser "Cliente" ou "Funcionário".
+ * @returns {void} Nao retorna nada, apenas realiza o atendimento.
+ *
+ * @example Realizando o atendimento
+ * ```ts
+ * atendimento("Cliente");
+ * // Realiza o atendimento do usuário como cliente.
+ *
+ * atendimento("Funcionário");
+ * // Realiza o atendimento do usuário como funcionário.
+ * ```
  */
-function atendimento(usr) {}
+function atendimento(usr) {
+  switch (usr) {
+    case "Cliente":
+      break;
+    case "Funcionário":
+      break;
+    default:
+      console.error("Usuário inválido:" + usr);
+      Deno.exit(1);
+  }
+}
