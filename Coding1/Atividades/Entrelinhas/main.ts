119fdc8017474eb9e6f05bbee1c3afcb8d79ce72
Kacaii <kacaii.privado@gmail.com>
2024-11-30 13:52:22 -0300

    squash files

--- Coding1/Atividades/Entrelinhas/main.ts
+++ Coding1/Atividades/Entrelinhas/main.ts
@@ -1,11 +1,58 @@
-import { TELA_PRINCIPAL, TELA_CLIENTE } from "./Telas/TELA_PRINCIPAL.ts";
-
-// type Usuario = "Cliente" | "Profissional"; // Vou usar isso depois
 type NomeTela = "TelaPrincipal" | "TelaCliente" | "TelaProfissional";
 
-export type Tela = {
+type Tela = {
   nome: NomeTela;
   conteudo: string;
+  executarFuncionalidade: () => void;
+};
+
+const TELA_PRINCIPAL: Tela = {
+  nome: "TelaPrincipal",
+  conteudo: `
+███████╗███╗   ██╗████████╗██████╗ ███████╗██╗     ██╗███╗   ██╗██╗  ██╗ █████╗ ███████╗
+██╔════╝████╗  ██║╚══██╔══╝██╔══██╗██╔════╝██║     ██║████╗  ██║██║  ██║██╔══██╗██╔════╝
+█████╗  ██╔██╗ ██║   ██║   ██████╔╝█████╗  ██║     ██║██╔██╗ ██║███████║███████║███████╗
+██╔══╝  ██║╚██╗██║   ██║   ██╔══██╗██╔══╝  ██║     ██║██║╚██╗██║██╔══██║██╔══██║╚════██║
+███████╗██║ ╚████║   ██║   ██║  ██║███████╗███████╗██║██║ ╚████║██║  ██║██║  ██║███████║
+╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+========================================================================================
+
+  = MENU PRINCIPAL = Selecione um usuário ⬇️
+
+  1 - Profissional
+  2 - Cliente
+  3 - Sair
+                                                                       Pedro Ayres, 2024
+========================================================================================
+`,
+  executarFuncionalidade() {
+    const usuarioSelecionado = prompt("\nEscolha");
+
+    switch (usuarioSelecionado) {
+      case "1": {
+        exibirTela("TelaCliente");
+        break;
+      }
+      case "2": {
+        exibirTela("TelaProfissional");
+        break;
+      }
+    }
+  },
+};
+
+// TODO: Implementar TELA_CLIENTE  '
+export const TELA_CLIENTE: Tela = {
+  nome: "TelaCliente",
+  conteudo: `== Tela Cliente ==`,
+  executarFuncionalidade() {},
+};
+
+// TODO: Implementar TELA_PROFISSIONAL  '
+export const TELA_PROFISSIONAL: Tela = {
+  nome: "TelaProfissional",
+  conteudo: `== Tela Profissional ==`,
+  executarFuncionalidade() {},
 };
 
 /**
@@ -20,19 +67,15 @@ const MapaDeTelas = new Map<NomeTela, Tela>([
 
 /**
  * Exibe a tela passando seu nome com argumento
- * O script busca o nome no {@linkcode MapaDeTelas} e exibe o conteudo no console.
+ * O script busca o nome no {@linkcode MapaDeTelas} e exibe o conteúdo no console.
  */
 function exibirTela(tela: NomeTela) {
-  console.log(MapaDeTelas.get(tela)!.conteudo);
+  console.log(MapaDeTelas.get(tela)?.conteudo);
+  MapaDeTelas.get(tela)?.executarFuncionalidade();
 }
 
-// TODO: Implementar funcionalidade principal  '
-function main() {
-  exibirTela("TelaPrincipal");
-  prompt("> ");
-}
-
+// Inicia o script
 if (import.meta.main) {
   console.clear(); // Limpe a tela antes de tudo
-  main();
+  exibirTela("TelaPrincipal");
 }
