398240bbc06ce45742f77a6797cb43265eb10aeb
Kacaii <kacaii.privado@gmail.com>
2024-11-30 11:12:35 -0300

    rewrite gui

---
+++ Coding1/Atividades/Entrelinhas/main.ts
@@ -0,0 +1,51 @@
+type Usuario = "Cliente" | "Profissional"; // Vou usar isso depois
+type NomeTela = "TelaPrincipal" | "TelaCliente";
+type Tela = {
+  nome: NomeTela;
+  conteudo: string;
+};
+
+const TELA_PRINCIPAL: Tela = {
+  nome: "TelaPrincipal",
+  conteudo: `
+███████╗███╗   ██╗████████╗██████╗ ███████╗██╗     ██╗███╗   ██╗██╗  ██╗ █████╗ ███████╗
+██╔════╝████╗  ██║╚══██╔══╝██╔══██╗██╔════╝██║     ██║████╗  ██║██║  ██║██╔══██╗██╔════╝
+█████╗  ██╔██╗ ██║   ██║   ██████╔╝█████╗  ██║     ██║██╔██╗ ██║███████║███████║███████╗
+██╔══╝  ██║╚██╗██║   ██║   ██╔══██╗██╔══╝  ██║     ██║██║╚██╗██║██╔══██║██╔══██║╚════██║
+███████╗██║ ╚████║   ██║   ██║  ██║███████╗███████╗██║██║ ╚████║██║  ██║██║  ██║███████║
+╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+========================================================================================
+
+  1 - Profissional
+  2 - Cliente
+  3 - Sair
+                                                                       Pedro Ayres, 2024
+========================================================================================
+`,
+};
+
+/**
+ * Mapa contendo todas as telas do script
+ * Use `MapaDeTelas.get()` para acessar seus conteudo.
+ */
+const MapaDeTelas = new Map<NomeTela, Tela>([
+  ["TelaPrincipal", TELA_PRINCIPAL],
+]);
+
+/**
+ * Exibe a tela passando seu nome com argumento
+ * O script busca o nome no {@linkcode MapaDeTelas} e exibe o conteudo no console.
+ */
+function exibirTela(tela: NomeTela) {
+  console.log(MapaDeTelas.get(tela)?.conteudo);
+}
+
+function main() {
+  console.clear();
+  exibirTela("TelaPrincipal");
+  prompt("");
+}
+
+if (import.meta.main) {
+  main();
+}
